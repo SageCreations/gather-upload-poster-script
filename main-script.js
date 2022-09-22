2956f143ae24a8133d757c731e8509574a30a8f7
SageCreations <sagecreations.ec@gmail.com>
2022-09-22 13:38:22 -0500

    trying to figure out function calling

--- main-script.js
+++ main-script.js
@@ -11,106 +11,113 @@ import fs from "fs";
 
 
 // STEP # 1
-readCSV(CSV_FILE); // calls the function in ./read-csv-file.js
-
-// makes an object out of the exported json file of the csv that was created by the readCSV function.
-import csv_json_object from './csv-data.json' assert { type: "json" };
+async function main() {
+    await readCSV(CSV_FILE);
 
 
-// STEP # 2
-let map_id_array = [] // declares and initializes an empty map_id_array
-// for loop that goes through the entire csv list
-for (let i = 0; i < csv_json_object.length; i++) {
+    //readCSV(CSV_FILE) // calls the function in ./read-csv-file.js
 
-    // creates a obj variable based on the current object of the csv_array
-    let obj = csv_json_object[i];
+    // makes an object out of the exported json file of the csv that was created by the readCSV function.
+    //import csv_json_object from './csv-data.json' assert { type: "json" };
+    var csv_json_object = [];
+    console.log(typeof csv_json_object);
 
-    var check = false; //used for checking map_id repeats
+    // STEP # 2
+    let map_id_array = [] // declares and initializes an empty map_id_array
+    // for loop that goes through the entire csv list
+    for (let i = 0; i < csv_json_object.length; i++) {
 
-    // checks to see if there even is map_id listed
-    if (obj.MAP_ID !== null) {
-        // loops through the map_id_array
-        for (var j in map_id_array) {
-            // check to see if the current obj.map_id is already in it.
-            if (obj.MAP_ID == map_id_array[j]) {
-                check = true; // check is now true if there is a repeated MAP_ID
-                break; // get out of this current for loop.
-            }
-        }
-        // if the for loop exits and check is still false... 
-        if (check == false) {
-            map_id_array.push(obj.MAP_ID); // ...add the map_id to the map_id_array.
-        }
-    }
-}
+        // creates a obj variable based on the current object of the csv_array
+        let obj = csv_json_object[i];
 
-// logs the map_id_array to the console
-//console.log(map_id_array);
+        var check = false; //used for checking map_id repeats
 
-// STEP # 3
-// traverse the map_id_array
-for (var i in map_id_array) {
-    // creates a json file for each the room's object data
-    getMapData(API_KEY, SPACE_ID, map_id_array[i]);
-}
-
-
-// STEP # 4
-
-for (var i in map_id_array) {
-
-    var map_data = JSON.parse(
-        fs.readFileSync(
-            `./${encodeURIComponent(map_id_array[i])}-data.json`
-        )
-    );
-
-    var obj = map_data.objects; // we are only wanting the objects of the room
-    
-    for (var j in csv_json_object) {
-        
-        var csv_obj = csv_json_object[j];
-        
-        
-        if (csv_obj.MAP_ID == map_id_array[i]) {
-            
-            
-    
-            //console.log(map_id_array[i]);
-            
-
-            for (var k in obj) {
-                
-                if (obj[k].previewMessage == csv_obj.ID) {
-                
-                    //TODO: need to find a way to abstract this more (currently working)
-                    if (obj[k]._name == "Bulletin (Video)") {
-                        obj[k].properties.video = csv_obj.Video; 
-                    } 
-
-                    if (obj[k]._name == "face" || obj[k]._name == "not") {
-                        obj[k].properties.url = csv_obj.PDF;
-                    }
-
-
-                     
-                    // JSON data name = Gathertown ui label
-                    // properties.video = video link
-                    // properties.url = pdf link
-                    // normal = Object Image (when the player is not in range)
-                    // highlighted = active image (what displays when a player is in interact range)
-                    // properties.blurb = caption (only available for images)
-                    // preview = idk
-
-                    
-
+        // checks to see if there even is map_id listed
+        if (obj.MAP_ID !== null) {
+            // loops through the map_id_array
+            for (var j in map_id_array) {
+                // check to see if the current obj.map_id is already in it.
+                if (obj.MAP_ID == map_id_array[j]) {
+                    check = true; // check is now true if there is a repeated MAP_ID
+                    break; // get out of this current for loop.
                 }
             }
+            // if the for loop exits and check is still false... 
+            if (check == false) {
+                map_id_array.push(obj.MAP_ID); // ...add the map_id to the map_id_array.
+            }
         }
-        
     }
-    
-    //console.log(obj);
-    setMapData(SPACE_ID, map_id_array[i], API_KEY, obj);
+
+    // logs the map_id_array to the console
+    //console.log(map_id_array);
+
+    // STEP # 3
+    // traverse the map_id_array
+    for (var i in map_id_array) {
+        // creates a json file for each the room's object data
+        getMapData(API_KEY, SPACE_ID, map_id_array[i]);
+    }
+
+
+    // STEP # 4
+    // loop through map_id_array
+    for (var i in map_id_array) {
+
+        var map_data = JSON.parse(
+            fs.readFileSync(
+                `./${encodeURIComponent(map_id_array[i])}-data.json`
+            )
+        );
+
+        var obj = map_data.objects; // we are only wanting the objects of the room
+
+        for (var j in csv_json_object) {
+
+            var csv_obj = csv_json_object[j];
+
+
+            if (csv_obj.MAP_ID == map_id_array[i]) {
+
+
+
+                //console.log(map_id_array[i]);
+
+
+                for (var k in obj) {
+
+                    if (obj[k].previewMessage == csv_obj.ID) {
+
+                        //TODO: need to find a way to abstract this more (currently working)
+                        if (obj[k]._name == "Bulletin (Video)") {
+                            obj[k].properties.video = csv_obj.Video;
+                        }
+
+                        if (obj[k]._name == "sideview" || obj[k]._name == "not" || obj[k]._name == "face" || obj[k]._name == "untitled") {
+                            obj[k].properties.url = csv_obj.PDF;
+                        }
+
+
+
+                        // JSON data name = Gathertown ui label
+                        // properties.video = video link
+                        // properties.url = pdf link
+                        // normal = Object Image (when the player is not in range)
+                        // highlighted = active image (what displays when a player is in interact range)
+                        // properties.blurb = caption (only available for images)
+                        // preview = idk
+
+
+
+                    }
+                }
+            }
+
+        }
+
+
+        setMapData(SPACE_ID, map_id_array[i], API_KEY, obj);
+    }
 }
 
+main();
--- read-csv-file.js
+++ read-csv-file.js
@@ -4,25 +4,32 @@ import csv from 'jquery-csv';
 
 //let csv_objects = [];
 export function readCSV(CSV_FILE) {
-    // this snippet here reads the csv file passed in from config and spits it as a json.
-    fs.readFile(CSV_FILE, 'UTF-8', function (err, csv_file) {
-        if (err) { console.log(err); }
-        csv.toObjects(csv_file, {}, function (err, data) {
+
+    return new Promise((resolve, reject) => 
+        fs.readFile(CSV_FILE, 'UTF-8', function (err, csv_file) {
             if (err) { console.log(err); }
-            fs.writeFile(
-                "csv-data.json",
-                JSON.stringify(data, null, "\t"),
-                (err) => {
-                    // Error checking
-                    if (err) throw err;
-                    //console.log(data);
-                }
-            );
-        });
-    });
+            csv.toObjects(csv_file, {}, function (err, data) {
+                if (err) { console.log(err); }
+                fs.writeFile(
+                    "csv-data.json",
+                    JSON.stringify(data, null, "\t"),
+                    (err) => {
+                        // Error checking
+                        if (err) throw err;
+                        console.log(data);
+                    }
+                )
+            });
+        })
+    );
 
+    // this snippet here reads the csv file passed in from config and spits it as a json.
+
+
+
+    //console.log(typeof JSON.parse(fs.readFileSync('csv-data.json')));
+    //return './csv-data.json';
 }
-
 
 
 
